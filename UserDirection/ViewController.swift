5550cc55ad0b8a535d0d16cd62ce30d71fad928d
Alexander Nikolaychuk <alexksnikol@gmail.com>
2020-05-23 17:45:58 +0300

    finished corelocation part

--- UserDirection/ViewController.swift
+++ UserDirection/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    private let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
 
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        guard let location = locations.last else {
+            return
+        }
+        let point = CLLocationCoordinate2D(latitude: location.coordinate.latitude,
+                                           longitude: location.coordinate.longitude)
+        print("Location \(point), Direction Angle \(location.course)")
+    }
+    
+}
